1e634a1c7a480408c2b78af3997436b6e731b74c
Ben James <benjames171@gmail.com>
2024-07-05 16:22:19 +0100

    update

---
+++ game/object/cloud.go
@@ -0,0 +1,38 @@
+components {
+  id: "cloud"
+  component: "/game/object/cloud.script"
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
+embedded_components {
+  id: "sprite"
+  type: "sprite"
+  data: "default_animation: \"cloud1\"\n"
+  "material: \"/builtins/materials/sprite.material\"\n"
+  "blend_mode: BLEND_MODE_ALPHA\n"
+  "textures {\n"
+  "  sampler: \"texture_sampler\"\n"
+  "  texture: \"/main/main.atlas\"\n"
+  "}\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
--- game/object/explosion.go
+++ game/object/explosion.go
@@ -1,6 +1,6 @@
 components {
-  id: "missile"
-  component: "/game/object/missile.script"
+  id: "pfx"
+  component: "/game/object/explosion.particlefx"
   position {
     x: 0.0
     y: 0.0
@@ -14,8 +14,8 @@ components {
   }
 }
 components {
-  id: "missile1"
-  component: "/game/object/missile.particlefx"
+  id: "script"
+  component: "/game/object/explosion.script"
   position {
     x: 0.0
     y: 0.0
@@ -32,19 +32,18 @@ embedded_components {
   id: "co"
   type: "collisionobject"
   data: "collision_shape: \"\"\n"
-  "type: COLLISION_OBJECT_TYPE_KINEMATIC\n"
-  "mass: 0.0\n"
+  "type: COLLISION_OBJECT_TYPE_DYNAMIC\n"
+  "mass: 1.0\n"
   "friction: 0.1\n"
   "restitution: 0.5\n"
-  "group: \"missile\"\n"
-  "mask: \"city\"\n"
-  "mask: \"shot\"\n"
+  "group: \"explode\"\n"
+  "mask: \"missile\"\n"
   "embedded_collision_shape {\n"
   "  shapes {\n"
   "    shape_type: TYPE_SPHERE\n"
   "    position {\n"
-  "      x: -0.5\n"
-  "      y: 0.5\n"
+  "      x: 0.0\n"
+  "      y: 0.0\n"
   "      z: 0.0\n"
   "    }\n"
   "    rotation {\n"
@@ -57,35 +56,12 @@ embedded_components {
   "    count: 1\n"
   "    id: \"\"\n"
   "  }\n"
-  "  data: 4.0\n"
+  "  data: 16.0\n"
   "}\n"
   "linear_damping: 0.0\n"
   "angular_damping: 0.0\n"
   "locked_rotation: false\n"
   "bullet: false\n"
-  ""
-  position {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-  }
-  rotation {
-    x: 0.0
-    y: 0.0
-    z: 0.0
-    w: 1.0
-  }
-}
-embedded_components {
-  id: "sprite"
-  type: "sprite"
-  data: "default_animation: \"missile\"\n"
-  "material: \"/builtins/materials/sprite.material\"\n"
-  "blend_mode: BLEND_MODE_ALPHA\n"
-  "textures {\n"
-  "  sampler: \"texture_sampler\"\n"
-  "  texture: \"/game/core/game.tilesource\"\n"
-  "}\n"
   ""
   position {
     x: 0.0
