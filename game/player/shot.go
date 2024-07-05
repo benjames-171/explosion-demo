731f3af9e0b63ad35ecef67d5fa6b8b08aa0a82d
Ben James <benjames171@gmail.com>
2024-07-05 17:15:05 +0100

    update

--- game/player/shot.go
+++ game/player/shot.go
@@ -98,3 +98,22 @@ embedded_components {
     w: 1.0
   }
 }
+embedded_components {
+  id: "exp-fac"
+  type: "factory"
+  data: "prototype: \"/game/object/explosion.go\"\n"
+  "load_dynamically: false\n"
+  "dynamic_prototype: false\n"
+  ""
+  position {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+  }
+  rotation {
+    x: 0.0
+    y: 0.0
+    z: 0.0
+    w: 1.0
+  }
+}
